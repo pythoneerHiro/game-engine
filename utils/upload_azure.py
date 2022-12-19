61fa2ab7913d4f2b181ae411135f3e5ad3e72b5c
Hiro <laciferin@gmail.com>
2022-12-19 08:56:47 +0530

    :sparkles: upload azue
    Signed-off-by: Hiro <laciferin@gmail.com>

--- utils/upload_azure.py
+++ utils/upload_azure.py
@@ -1,16 +1,19 @@
 def uploadAzureBlobStorage(bucketName: str, file: str) -> str:
     pass
 
-# from azure.storage.blob import BlobServiceClient
-#
-# storage_account_key = "GRAB_IT_FROM_AZURE_PORTAL"
-# storage_account_name = "GRAB_IT_FROM_AZURE_PORTAL"
-# connection_string = "GRAB_IT_FROM_AZURE_PORTAL"
-# container_name = "GRAB_IT_FROM_AZURE_PORTAL"
-#
-# def uploadAzureBlobStorage(file_path,file_name):
-#     blob_service_client = BlobServiceClient.from_connection_string(connection_string)
-#     blob_client = blob_service_client.get_blob_client(container=container_name, blob=file_name)
-#     with open(file_path,”rb”) as data:
-#         blob_client.upload_blob(data)
-#         print(f”Uploaded {file_name}.”)
+
+from azure.storage.blob import BlobServiceClient
+
+storage_account_key = "RfkDC1yVo40Wiixh6QkrH4hqm7gWPsx8b9F+WWIu1yONJLDReaA+EkdbCdR/lnNER4Prvx80Hp9w+AStNp4eqQ=="
+storage_account_name = "bzdevstorageaccount"
+connection_string = "DefaultEndpointsProtocol=https;AccountName=bzdevstorageaccount;AccountKey=RfkDC1yVo40Wiixh6QkrH4hqm7gWPsx8b9F+WWIu1yONJLDReaA+EkdbCdR/lnNER4Prvx80Hp9w+AStNp4eqQ==;EndpointSuffix=core.windows.net"
+container_name = "game-engine"
+
+
+def uploadAzureBlobStorage(file_path, file_name) -> str:
+    blob_service_client = BlobServiceClient.from_connection_string(connection_string)
+    blob_client = blob_service_client.get_blob_client(container=container_name, blob=file_name)
+    with open(file_path, "rb") as data:
+        blob_client.upload_blob(data)
+        print(f"Uploaded {file_name}.")
+    return blob_client.url
