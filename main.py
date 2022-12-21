df8dda391c30f6c6bdfb2f93eba913263176b42b
Hiro <pythoneerHiro@gmail.com>
2022-12-21 23:27:32 +0530

    :loud_sound: logging
    Signed-off-by: Hiro <pythoneerHiro@gmail.com>

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
 from os import environ
 
 from fastapi import FastAPI
+from fastapi.logger import logger
+from icecream import ic
+from requests import Request
 
 from routers import image_svc
 
 debug = environ.get("debug", False)
 
 app = FastAPI(debug=debug)
+
 app.include_router(image_svc.router, prefix="/image")
 app.include_router(image_svc.router1, prefix="/image/v1")
+
+
+@app.middleware("http")
+async def log_stuff(request: Request, call_next):
+    logger.info(f"{request.method} {request.url}")
+    req = await request.body()
+    ic("Request", req)
+    response = await call_next(request)
+    logger.info(response.status_code)
+    return response
 
 
 @app.get("/")
